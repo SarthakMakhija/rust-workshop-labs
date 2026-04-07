a009f3e83a0984b2f4026ec657cd23adb9332029
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-04-07 18:48:16 +0530

    Sarthak | Starts adding stage 13

---
+++ crates/stage13/src/cache.rs
@@ -0,0 +1,107 @@
+use crate::shard::{Ref, Shard};
+use std::borrow::Borrow;
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::sync::Arc;
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::Duration;
+
+type Shards<K: Hash + Eq + Clone, V> = Arc<Vec<Shard<K, V>>>;
+
+struct Cache<K, V>
+where
+    K: Hash + Eq + Clone + Send + Sync + 'static,
+    V: Send + Sync + 'static,
+{
+    shards: Shards<K, V>,
+    num_shards: usize,
+}
+
+impl<K, V> Cache<K, V>
+where
+    K: Hash + Eq + Clone + Send + Sync + 'static,
+    V: Send + Sync + 'static,
+{
+    fn new(num_shards: usize) -> Arc<Self> {
+        // TODO: Implement sharded initialization.
+        let shards: Shards<K, V> = Arc::new((0..num_shards).map(|_| Shard::new()).collect());
+        Self::spawn_expired_keys_cleaner(shards.clone());
+        Arc::new(Self {
+            shards,
+            num_shards
+        })
+    }
+
+    fn put(&self, key: K, value: V, ttl: Duration) {
+        // TODO: Implement sharded insertion by delegating to the correct Shard.
+        //unimplemented!()
+        self.shards[self.shard_index(&key)].put(key, value, ttl);
+    }
+
+    // 🚀 High-performance Lookup.
+    fn get<Q>(&self, key: &Q) -> Option<Ref<'_, K, V>>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        // TODO: Implement lookup by delegating to the Shard.
+        // unimplemented!()
+        self.shards[self.shard_index(&key)].get(key)
+    }
+
+    fn shard_index<Q>(&self, key: &Q) -> usize
+    where
+        Q: Hash + ?Sized,
+    {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        (hasher.finish() as usize) % self.num_shards
+    }
+
+    fn spawn_expired_keys_cleaner(shards: Shards<K, V>) -> JoinHandle<()> {
+        thread::spawn(move || {
+            let mut shard_index = 0;
+            loop {
+                // 💡 Strategy: Clean one shard, sleep, then move to the next.
+                // This spreads the "Cleanup Tax" over time.
+                let shard = shards.get(shard_index).unwrap();
+                shard.cleanup();
+
+                shard_index = (shard_index + 1) % shards.len();
+                thread::sleep(Duration::from_secs(1));
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    #[test]
+    fn attempt_get_a_key_from_empty_cache() {
+        let cache: Arc<Cache<String, String>> = Cache::new(8);
+        assert!(cache.get("test").is_none());
+    }
+
+    #[test]
+    fn get_existing_key() {
+        let cache = Cache::new(8);
+        cache.put(String::from("rustconf"), String::from("2026"), Duration::from_secs(1));
+
+        let value = cache.get("rustconf").unwrap();
+        assert_eq!(*value, "2026");
+    }
+
+    #[test]
+    fn get_expired_key() {
+        let cache = Cache::new(8);
+        cache.put(String::from("rustconf"), String::from("2026"), Duration::from_millis(2));
+
+        thread::sleep(Duration::from_millis(5));
+
+        let value = cache.get("rustconf");
+        assert!(value.is_none());
+    }
+}
---
+++ crates/stage13/src/lib.rs
@@ -0,0 +1,2 @@
+mod cache;
+pub(crate) mod shard;
